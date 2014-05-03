45c207ac47474230dae732e419e5b909770c709a
Mayur <mmehta10@ufl.edu>
2014-05-03 16:00:04 -0400

    fixed duplicate image and incorrect dimension issue

--- ImAdd.py
+++ ImAdd.py
@@ -5,28 +5,27 @@ import ImgIO
 def add(image1, image2):  # add two images together
     if image1.width == image2.width and image1.height == image2.height:
         return_list = []
-        return_list = [sum(x) for x in zip(image1.value_list, image2.value_list)]
-        for i in range(0, len(return_list)):
-            if 0 <= return_list[i] <= 255:
-                pass
+        for i in range(0, len(image1.value_list)):
+            tmp = image2.value_list[i] + image1.value_list[i]
+            if 0 <= tmp <= 255:
+                return_list.append(tmp)
             else:
-                return_list[i] = 255
+                return_list.append(tmp % 255)  # loop values around if saturation
         return return_list
     else:
         print "Error: image dimensions do not match!"
-        #return 0
 
 
 def main():  # test case
     print('start!!!!!')
     ima = ImgIO.ImgIO()
     imb = ImgIO.ImgIO()
-    ima.read_image("test.jpg")
+    ima.read_image("test.png")
     imb.read_image("new.png")
     add_list = add(ima, imb)
     imc = ImgIO.ImgIO()
-    imc.read_list(add_list, "final.png", ima.width, ima.height)
-    imc.write_image()
+    imc.read_list(add_list, "final1.png", ima.width, ima.height)
+    imc.write_image("final1.png")
 
 
 if __name__ == '__main__':
--- ImgIO.py
+++ ImgIO.py
@@ -2,20 +2,20 @@ __author__ = 'Mayur Mehta'
 #handles the input and output of images
 #images are converted to grayscale 0 to 255 value set
 #jpg are converted to png
-import Image
+import Image  # using PIL
 
 
 class ImgIO:
-    value_list = []
-    height = 0
     width = 0
-    image_name = "none.png"
+    height = 0
+    image_name = "none"
+    value_list = []  # stores image values
 
     def __init__(self):
-        value_list = []
-        height = 0
-        width = 0
-        image_name = "none.png"
+        self.value_list = []
+        self.height = 0
+        self.width = 0
+        self.image_name = "none.png"
 
     def read_image(self, image_name):  # image name --- xxx.ext
         im = Image.open(image_name).convert('LA')  # Can be many different formats.
@@ -23,20 +23,22 @@ class ImgIO:
         self.width = im.size[0]
         self.height = im.size[1]
         self.image_name = image_name
+        tmplist = []
         for i in range(0, im.size[0]):
             for j in range(0, im.size[1]):
-                self.value_list.append(pix[i, j][0])
+                tmplist.append(pix[i, j][0])
+        self.value_list = tmplist
 
-    def write_image(self):
+    def write_image(self, name):
         im = Image.new('LA', (self.width, self.height))
         pix = im.load()
         for i in range(0, self.width):
             for j in range(0, self.height):
                 pix[i, j] = (self.value_list[i * self.height + j], 255)
-        im.save(self.image_name)
+        im.save(name)
 
     def read_list(self, input_list, name, width, height):
-        if len(input_list) == 2 * width * height:
+        if len(input_list) == width * height:
             self.value_list = input_list
             self.width = width
             self.height = height
@@ -48,8 +50,8 @@ class ImgIO:
 def main():  # test case
     print('start!!!!!')
     testobj = ImgIO()
-    testobj.read_image("test.jpg")
-    testobj.write_image("new.png")
+    testobj.read_image("new.jpg")
+    testobj.write_image("new1.png")
 
 
 if __name__ == '__main__':
